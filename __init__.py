97eb1723bfaac9833dbf247a8c1dd962ebd050be
kuailefengnan2024 <han552401231@gmail.com>
2025-09-05 19:32:15 +0800

    save

--- __init__.py
+++ __init__.py
@@ -1,3 +1,14 @@
-from .comfyui_vision_plugin import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+# 导入节点类
+from .comfyui_vision_plugin import VisionAPIPluginNode
+
+# 创建节点类映射
+NODE_CLASS_MAPPINGS = {
+    "VisionAPIPluginNode": VisionAPIPluginNode
+}
+
+# 创建节点显示名称映射
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "VisionAPIPluginNode": "Vision API Node"
+}
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
